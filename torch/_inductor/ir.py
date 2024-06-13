2647280623d8afa5562e551bdb03119239baccdc
Isuru Fernando <ifernando@quansight.com>
2024-06-13 13:00:23 +0000

    Add a shape property to IR nodes
    ghstack-source-id: de62b975c98dbd06c55d22d40ae2fc81d0fd35c1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127818

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -319,6 +319,10 @@ class IRNode:
     def get_size(self):
         raise NotImplementedError(f"get_size() is not implemented by {type(self)}!")
 
+    @property
+    def shape(self):
+        return self.get_size()
+
     def get_numel(self):
         return sympy_product(self.get_size())
 
@@ -3191,7 +3195,11 @@ class NoneAsConstantBuffer(IRNode):
 class ShapeAsConstantBuffer(IRNode):
     def __init__(self, shape):
         super().__init__()
-        self.shape = shape
+        self._shape = shape
+
+    @property
+    def shape(self):
+        return self._shape
 
     def get_unbacked_symbol_uses(self) -> Set[sympy.Symbol]:
         return free_unbacked_symbols(self.shape)
